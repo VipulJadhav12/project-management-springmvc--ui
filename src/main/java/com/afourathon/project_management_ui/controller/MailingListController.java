2428495afefee2159de7b3268fc6b26a1ba29e2a
VipulJadhav12 <vipulsjadhav.12@gmail.com>
2023-01-05 09:04:47 +0530

    Added thymeleaf based form level constraints and validations. Added pop-up alerts and dialog box for Project and MailingList related CRUD operations.

--- src/main/java/com/afourathon/project_management_ui/controller/MailingListController.java
+++ src/main/java/com/afourathon/project_management_ui/controller/MailingListController.java
@@ -3,15 +3,18 @@ package com.afourathon.project_management_ui.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.afourathon.project_management_ui.data.entity.MailingList;
 import com.afourathon.project_management_ui.data.payloads.request.MailingListRequest;
+import com.afourathon.project_management_ui.data.payloads.response.ApiResponse;
 import com.afourathon.project_management_ui.service.ConsumeMailingListRestApiService;
 
 @Controller
@@ -39,8 +42,13 @@ public class MailingListController {
 	}
 	
 	@PostMapping("/addEmail")
-	public String addEmail(@ModelAttribute MailingListRequest emailRequest) {
-		mailingListService.addEmail(emailRequest);
+	public String addEmail(@ModelAttribute MailingListRequest emailRequest, RedirectAttributes redirAttrs) {
+		ApiResponse apiResponse = mailingListService.addEmail(emailRequest);
+		
+		if(null == apiResponse || apiResponse.getStatus() != HttpStatus.CREATED)
+			redirAttrs.addFlashAttribute("error", "An error occured while adding new email!");
+		else
+			redirAttrs.addFlashAttribute("success", "New email was added successfully!");
 		
 		return "redirect:/displayAllMailingList";
 	}
@@ -61,8 +69,14 @@ public class MailingListController {
 	}
 	
 	@PostMapping("/updateEmail")
-	public String updateEmail(@RequestParam Long mailId, @ModelAttribute MailingListRequest emailRequest) {
-		mailingListService.updateEmail(mailId, emailRequest);
+	public String updateEmail(@RequestParam Long mailId, @ModelAttribute MailingListRequest emailRequest, 
+			RedirectAttributes redirAttrs) {
+		ApiResponse apiResponse = mailingListService.updateEmail(mailId, emailRequest);
+		
+		if(null == apiResponse || apiResponse.getStatus() != HttpStatus.OK)
+			redirAttrs.addFlashAttribute("error", "An error occured while updating email!");
+		else
+			redirAttrs.addFlashAttribute("success", "Email was updated successfully!");
 		
 		return "redirect:/displayAllMailingList";
 	}
--- src/main/java/com/afourathon/project_management_ui/data/payloads/request/ProjectRequest.java
+++ src/main/java/com/afourathon/project_management_ui/data/payloads/request/ProjectRequest.java
@@ -1,8 +1,6 @@
 package com.afourathon.project_management_ui.data.payloads.request;
 
 import java.io.Serializable;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,19 +13,14 @@ public class ProjectRequest implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 
-	@NotBlank(message = "Project name cannot be null.")
 	private String name;
 	
-	@NotBlank(message = "Project's start date cannot be null.")
 	private String startDate;
 	
 	private String endDate;
 	
-	@NotBlank(message = "Manager name cannot be null.")
 	private String managerName;
 	
-	@NotBlank(message = "Manager email cannot be null.")
-	@Email
 	private String managerEmail;
 	
 }
